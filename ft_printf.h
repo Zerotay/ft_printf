ada20ea7eee6ef97c31ee3522cbdd4b3633c1515
Zerotay <ubuntu@example.com>
2021-02-01 15:46:49 +0900

    hell

--- ft_printf.h
+++ ft_printf.h
@@ -13,10 +13,10 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 typedef struct	s_condition_for_conversion
 {
@@ -30,6 +30,7 @@ typedef struct	s_condition_for_conversion
 int				ft_printf(const char *s, ...);
 t_cfc			makecfc(const char *s, int *i, t_cfc cfc, va_list ap);
 int				print(t_cfc cfc, va_list ap);
+
 int				godi(t_cfc cfc, va_list ap);
 int				gox(t_cfc cfc, va_list ap);
 int				goxx(t_cfc cfc, va_list ap);
@@ -37,15 +38,19 @@ int				gou(t_cfc cfc, va_list ap);
 int				goc(t_cfc cfc, va_list ap);
 int				gos(t_cfc cfc, va_list ap);
 int				gop(t_cfc cfc, va_list ap);
+
 char			*itop(unsigned long n);
 char			*itoplus(t_cfc cfc, unsigned int n);
 char			*checkito(t_cfc cfc, int val);
+
 int				lengthplus(t_cfc cfc, unsigned long val);
 int				length(int val);
+
 int				justwrite(t_cfc cfc, unsigned long val);
 int				onlyfornull(t_cfc cfc);
 int				precisionlong(t_cfc cfc, int val, int len);
 int				sort(t_cfc cfc, int val, int len);
+
 size_t			ft_strlen(const char *s);
 int				ft_atoi(const char *str);
 char			*ft_itoa(int n);
