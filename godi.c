42c72b935aa4850902cb1fe218118cc3638fd90a
Zerotay <ubuntu@example.com>
2021-02-01 14:29:14 +0900

    wow

--- godi.c
+++ godi.c
@@ -19,7 +19,9 @@ static int		basic(t_cfc cfc, int val, int len)
 	int		i;
 
 	i = cfc.precision > len ? cfc.precision : len;
-	if (!(buf = ft_calloc(cfc.width + 1, 1)) || !(temp = checkito(cfc, val)))
+	if (!(buf = ft_calloc(cfc.width + 1, 1)))
+		return (-1);
+	if (!(temp = checkito(cfc, val)))
 		return (-1);
 	ft_memset(buf, ' ', cfc.width);
 	if (cfc.precision >= len)
@@ -46,7 +48,9 @@ int			zero(t_cfc cfc, int val, int len)
 	if (cfc.onlyfors && cfc.precision >= 0)
 		return (basic(cfc, val, len));
 	i = val < 0 ? cfc.width - len + 1 : cfc.width - len;
-	if (!(buf = ft_calloc(cfc.width + 1, 1)) || !(temp = ft_itoa(val)))
+	if (!(buf = ft_calloc(cfc.width + 1, 1)))
+		return (-1);
+	if (!(temp = ft_itoa(val)))
 		return (-1);
 	ft_memset(buf, '0', cfc.width);
 	buf[0] = val >= 0 ? buf[0] : '-';
@@ -66,20 +70,17 @@ int			godi(t_cfc cfc, va_list ap)
 	len = length(val);
 	if (!val && !cfc.precision)
 		return (onlyfornull(cfc));
-	else
-		if (cfc.width < len)
-			if (cfc.precision >= len)
-				return (precisionlong(cfc, val, len));
-			else
-				return (justwrite(cfc, val));
+	else if (cfc.width < len)
+		if (cfc.precision >= len)
+			return (precisionlong(cfc, val, len));
 		else
-			if (cfc.precision >= cfc.width)
-				return (precisionlong(cfc, val, len));
-			else
-				if (cfc.sort1zero2 == 1)
-					return (sort(cfc, val, len));
-				else if (cfc.sort1zero2 == 2)
-					return (zero(cfc, val, len));
-				else
-					return (basic(cfc, val, len));
+			return (justwrite(cfc, val));
+	else if (cfc.precision >= cfc.width)
+		return (precisionlong(cfc, val, len));
+	else if (cfc.sort1zero2 == 1)
+		return (sort(cfc, val, len));
+	else if (cfc.sort1zero2 == 2)
+		return (zero(cfc, val, len));
+	else
+		return (basic(cfc, val, len));
 }
--- gox.c
+++ gox.c
@@ -76,20 +76,17 @@ int			gox(t_cfc cfc, va_list ap)
 	len = lengthplus(cfc, val);
 	if (!val && !cfc.precision)
 		return (onlyfornull(cfc));
-	else
-		if (cfc.width < len)
-			if (cfc.precision >= len)
-				return (precisionlong(cfc, val, len));
-			else
-				return (justwrite(cfc, val));
+	else if (cfc.width < len)
+		if (cfc.precision >= len)
+			return (precisionlong(cfc, val, len));
 		else
-			if (cfc.precision >= cfc.width)
-				return (precisionlong(cfc, val, len));
-			else
-				if (cfc.sort1zero2 == 1)
-					return (sort(cfc, val, len));
-				else if (cfc.sort1zero2 == 2)
-					return (zero(cfc, val, len));
-				else
-					return (basic(cfc, val, len));
+			return (justwrite(cfc, val));
+	else if (cfc.precision >= cfc.width)
+		return (precisionlong(cfc, val, len));
+	else if (cfc.sort1zero2 == 1)
+		return (sort(cfc, val, len));
+	else if (cfc.sort1zero2 == 2)
+		return (zero(cfc, val, len));
+	else
+		return (basic(cfc, val, len));
 }
